18cca86200ca4d2332d8271571303b56596995a6
简律纯 <i@jyunko.cn>
2024-09-11 23:55:39 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import Layout from "@theme/Layout";
 import clsx from "clsx";
 import React from "react";
 import Benchmark from "../components/Benchmark";
-import AnimatedGradientStarWithGithub from "../components/MagicUi/animated-shiny-text";
+// import AnimatedGradientStarWithGithub from "../components/MagicUi/animated-shiny-text";
 import BlurFade from "../components/MagicUi/blur-fade";
 import BentoGridCard from "../components/MagicUi/card";
 import StarrySky from "../components/StarrySky";
