2041187a81678920c852a2d44e10f1a38f97fb9e
简律纯 <i@jyunko.cn>
2024-09-12 13:23:08 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://farmfe.org",
+  url: "https://hsiangnianian.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/farm-fe.github.io/",
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "farm-fe", // Usually your GitHub org/user name.
